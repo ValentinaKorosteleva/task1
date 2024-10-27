962bc4ae6530d982a6713b4d71c0bd21298d3a88
ValentinaKorosteleva <Valycha10.10@gmail.com>
2024-10-27 16:51:57 +0500

    Add files via upload

---
+++ hw1.py
@@ -0,0 +1,18 @@
+print((9**0.5)*5)    #1st program
+
+print (9.99 > 9.98 and 1000 != 1000.1) #2nd program
+
+a=(2*2+2)  #3rd program
+print(a)
+b=(2*(2+2))
+print(b)
+print(a==b)
+
+a='123.456'   #4th program
+print (type(a))
+a=float(a)
+print(type(a))
+a=a*10
+a=int(a)
+print(a)
+print(a%10)
